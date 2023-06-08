585e454120c3cc4da503086a4ebcc400da523abf
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-06-08 17:32:20 +0500

    custom checkox values

--- src/js/components/custom-select.js
+++ src/js/components/custom-select.js
@@ -1,6 +1,22 @@
 const select = document.getElementById("custom-select");
-if (select !== null) {
+const valueElem = select.querySelector(".hero__text-select");
+if (select !== null && valueElem !== null) {
 	select.addEventListener("click", () => {
-		select.classList.toggle("hero__container-input-opened");
+		select.classList.add("hero__container-input-opened");
+	});
+	document.addEventListener("click", (e) => {
+		if (e.target !== select) {
+			select.classList.remove("hero__container-input-opened");
+		}
+	});
+}
+
+const selectItems = select.querySelectorAll(".hero__item-select");
+if (selectItems.length > 0) {
+	selectItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			valueElem.setAttribute("data-value", item.getAttribute("data-value"));
+			valueElem.innerHTML = item.innerHTML;
+		});
 	});
 }
